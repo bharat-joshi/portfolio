fe5b4c80697b62cb25d354d617d71446cfb5a263
bharat-yosa <bharat.joshi@appsontechnologies.in>
2026-03-13 23:58:34 +0530

    Add footer link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,8 +8,8 @@ export default function Footer() {
     <footer id="contact" className={styles.footer}>
         
      <div className={styles.container}> 
-        {/* <div className={styles.content}> */}
-        <div className={styles.info}>
+        <div className={styles.content}>
+          <div className={styles.info}>
             <h3 className={styles.title}>Let's Connect</h3>
             <p className={styles.description}>
               I'm always open to discussing new projects, creative ideas, or opportunities to be part of your visions.
@@ -27,21 +27,21 @@ export default function Footer() {
             </div>
           </div> 
           
-          {/* <div className={styles.socials}>
+          <div className={styles.socials}>
             <h4 className={styles.socialTitle}>Social Profiles</h4>
             <div className={styles.socialLinks}>
-              <a href="#" target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
+              <a href="https://www.linkedin.com/in/bharat-joshi-9a40ba95?utm_source=share_via&utm_content=profile&utm_medium=member_ios" target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
                 LinkedIn
               </a>
-               <a href="#" target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
+               <a href="https://github.com/bharat-joshi" target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
                 GitHub
               </a>
-              <a href="#" target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
+              <a href="https://x.com/jb_bharat" target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
                 Twitter
               </a> 
             </div>
-          </div> */}
-        {/* </div> */}
+          </div>
+        </div>
         
         <div className={styles.bottom}>
           <p>&copy; {new Date().getFullYear()} Bharat Joshi. All Rights Reserved.</p>
